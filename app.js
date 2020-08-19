5058419c71711af5364cc8631e9ea44e359c213c
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-19 07:29:14 +0100

    Established basic home page using partials

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //https://developer.nytimes.com/
 //https://developer.nytimes.com/docs/books-product/1/overview
 //https://any-api.com/nytimes_com/books_api/docs/API_Description
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home', { pageTitle: 'Home'});
 });
 
 app.listen(port, () => {
