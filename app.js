2bb3b712f02b9b5fc8d2bc5d8da57b80edddd0d6
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-20 07:00:04 +0100

    Processed API call to capture key book info, display book info on home page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Home page, this will show a list of best selling books
 app.get('/', (req, res) => {
   let url = 'https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=';
   url += process.env.API_TOKEN;
@@ -17,8 +18,23 @@ app.get('/', (req, res) => {
   https.get(url, (response) => {
     response.on('data', (data) => {
       const bookData = JSON.parse(data);
+      const numOfResults = bookData.num_results; // Get the number of books returned in API call
+      let books = [];
 
-      res.render('home', { pageTitle: 'Home', numberOfResults: bookData.num_results });
+      let i;
+      for (i = 0; i < numOfResults; i += 1) {
+        const book = {
+          name: bookData.results[i].book_details[0].title,
+          description: bookData.results[i].book_details[0].description,
+          author: bookData.results[i].book_details[0].author,
+          amazonProductUrl: bookData.results[i].amazon_product_url,
+          rank: bookData.results[i].rank,
+          rankLastWeek: bookData.results[i].rank_last_week,
+        };
+
+        books.push(book);
+      }
+      res.render('home', { pageTitle: 'Home', books: books });
     });
   });
 });
