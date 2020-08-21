fba9224643c0645d37327cc7476971167ed30647
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-21 06:36:15 +0100

    Moved API code into its own module, implemented a promise to ensure data was returned before rendering page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ path: `${__dirname}/.env` });
 
+const nytAPI = require(`${__dirname}/public/js/nytAPI.js`);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
@@ -12,45 +14,15 @@ app.use(express.static('public'));
 
 // Home page, this will show a list of best selling books
 app.get('/', (req, res) => {
-  let url = 'https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=';
-  url += process.env.API_KEY;
+  const promiseToGetBooks = new Promise((resolve, reject) => {
+    const pageContent = nytAPI.getBestSellers();
+    setTimeout(() => {
+      resolve(pageContent);
+    }, 1000);
+  });
 
-  https.get(url, (response) => {
-    response.on('data', (data) => {
-      const bookData = JSON.parse(data);
-      const numOfResults = bookData.num_results; // Get the number of books returned in API call
-      const books = [];
-
-      let i;
-      for (i = 0; i < numOfResults; i += 1) {
-        // Assign some key book info to an object
-        let book = {
-          name: bookData.results[i].book_details[0].title,
-          description: bookData.results[i].book_details[0].description,
-          author: bookData.results[i].book_details[0].author,
-          amazonProductUrl: bookData.results[i].amazon_product_url,
-          rank: bookData.results[i].rank,
-          rankLastWeek: bookData.results[i].rank_last_week,
-          ISBN13: bookData.results[i].book_details[0].primary_isbn13,
-        };
-
-        let rankIcon;
-        if (book.rank > book.rankLastWeek) {
-          rankIcon = '⇧';
-        } else if (book.rank === book.rankLastWeek) {
-          rankIcon = '-';
-        } else if (book.rank < book.rankLastWeek) {
-          rankIcon = '⇩';
-        } else {
-          rankIcon = '(new entry)';
-        }
-
-        book.rankIcon = rankIcon;
-
-        books.push(book);
-      }
-      res.render('home', { pageTitle: 'Hardcover Fiction', books });
-    });
+  promiseToGetBooks.then((data) => {
+    res.render('home', data);
   });
 });
 
---
+++ public/js/nytAPI.js
@@ -0,0 +1,45 @@
+require('dotenv').config({ path: `${__dirname}/.env` });
+const https = require('https');
+
+exports.getBestSellers = () => {
+  let url = 'https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=';
+  const books = [];
+  url += process.env.API_KEY;
+
+  https.get(url, (response) => {
+    response.on('data', (data) => {
+      const bookData = JSON.parse(data);
+      const numOfResults = bookData.num_results; // Get the number of books returned in API call
+      let i;
+      for (i = 0; i < numOfResults; i += 1) {
+        // Assign some key book info to an object
+        const book = {
+          name: bookData.results[i].book_details[0].title,
+          description: bookData.results[i].book_details[0].description,
+          author: bookData.results[i].book_details[0].author,
+          amazonProductUrl: bookData.results[i].amazon_product_url,
+          rank: bookData.results[i].rank,
+          rankLastWeek: bookData.results[i].rank_last_week,
+          ISBN13: bookData.results[i].book_details[0].primary_isbn13,
+        };
+
+        let rankIcon;
+        if (book.rank > book.rankLastWeek) {
+          rankIcon = '⇧';
+        } else if (book.rank === book.rankLastWeek) {
+          rankIcon = '-';
+        } else if (book.rank < book.rankLastWeek) {
+          rankIcon = '⇩';
+        } else {
+          rankIcon = '(new entry)';
+        }
+
+        book.rankIcon = rankIcon;
+
+        books.push(book);
+      }
+    });
+  });
+  //return { books };
+  return { pageTitle: 'Hardcover Fiction', books }
+};
