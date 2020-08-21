68f34122852a614900d6676ce5148619954b6409
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-21 07:35:40 +0100

    Created function to return book genre/type lists, updated rank icon generation to cover cases where last rank was 0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: `${__dirname}/.env` });
 
-const nytAPI = require(`${__dirname}/public/js/nytAPI.js`);
+const nytAPI = require(`${__dirname}/public/js/nytAPI.js`); // Import API functions
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,8 +11,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Home page, this will show a list of best selling books
+const listNames = nytAPI.getListNames(); // Get a list of valid book list names
+
 app.get('/', (req, res) => {
+  // Promise utilised to allow enough time for data to be returned before rendering page
   const promiseToGetBooks = new Promise((resolve, reject) => {
     const pageContent = nytAPI.getBestSellers();
     setTimeout(() => {
--- public/js/nytAPI.js
+++ public/js/nytAPI.js
@@ -1,15 +1,31 @@
 require('dotenv').config({ path: `${__dirname}/.env` });
 const https = require('https');
 
+exports.getListNames = () => {
+  let url = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key='
+  url += process.env.API_KEY;
+
+  let listNames = [];
+
+  https.get(url, (response) => {
+    response.on('data', (data) => {
+      const listNamesData = JSON.parse(data);
+      listNames = listNamesData.results;
+    });
+  });
+  return listNames;
+};
+
 exports.getBestSellers = () => {
   let url = 'https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=';
-  const books = [];
   url += process.env.API_KEY;
+
+  const books = [];
 
   https.get(url, (response) => {
     response.on('data', (data) => {
       const bookData = JSON.parse(data);
-      const numOfResults = bookData.num_results; // Get the number of books returned in API call
+      const numOfResults = bookData.num_results;
       let i;
       for (i = 0; i < numOfResults; i += 1) {
         // Assign some key book info to an object
@@ -23,22 +39,23 @@ exports.getBestSellers = () => {
           ISBN13: bookData.results[i].book_details[0].primary_isbn13,
         };
 
+        // Work out which direction the book has moved in the charts, i.e. up down same or new entry
         let rankIcon;
-        if (book.rank > book.rankLastWeek) {
-          rankIcon = '⇧';
+        if (book.rankLastWeek === 0) {
+          book.rankIcon = '(new)';
+        } else if (book.rank > book.rankLastWeek) {
+          book.rankIcon = '⇧';
         } else if (book.rank === book.rankLastWeek) {
-          rankIcon = '-';
+          book.rankIcon = '-';
         } else if (book.rank < book.rankLastWeek) {
-          rankIcon = '⇩';
+          book.rankIcon = '⇩';
         } else {
-          rankIcon = '(new entry)';
+          book.rankIcon = '(new)';
         }
-
-        book.rankIcon = rankIcon;
 
         books.push(book);
       }
     });
   });
-  return { pageTitle: 'Hardcover Fiction', books }
+  return { pageTitle: 'Hardcover Fiction', books };
 };
