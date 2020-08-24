2d965761397bae71704f0d8e85f94f612311b76a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-24 07:21:32 +0100

    Fixed bug where undefined books were resulting in TypeErrors causing app crashes

--- public/js/nytAPI.js
+++ public/js/nytAPI.js
@@ -34,38 +34,41 @@ exports.getBestSellers = (listName, callback) => {
 
       let i;
       for (i = 0; i < numOfResults; i += 1) {
-        // Assign some key book info to an object
-        const book = {
-          name: bookData.results[i].book_details[0].title,
-          description: bookData.results[i].book_details[0].description,
-          author: bookData.results[i].book_details[0].author,
-          amazonProductUrl: bookData.results[i].amazon_product_url,
-          rank: bookData.results[i].rank,
-          rankLastWeek: bookData.results[i].rank_last_week,
-          ISBN13: bookData.results[i].book_details[0].primary_isbn13,
-          isbns: bookData.results[i].isbns,
-        };
+        console.log('***********')
+        if (bookData.results[i] !== undefined) {
+          // Assign some key book info to an object
+          const book = {
+            name: bookData.results[i].book_details[0].title,
+            description: bookData.results[i].book_details[0].description,
+            author: bookData.results[i].book_details[0].author,
+            amazonProductUrl: bookData.results[i].amazon_product_url,
+            rank: bookData.results[i].rank,
+            rankLastWeek: bookData.results[i].rank_last_week,
+            ISBN13: bookData.results[i].book_details[0].primary_isbn13,
+            isbns: bookData.results[i].isbns,
+          };
 
-        // Work out which direction the book has moved in the charts, i.e. up down same or new entry
-        let rankIcon;
-        if (book.rankLastWeek === 0) {
-          book.rankIcon = '(new)';
-        } else if (book.rank > book.rankLastWeek) {
-          book.rankIcon = '⇧';
-        } else if (book.rank === book.rankLastWeek) {
-          book.rankIcon = '-';
-        } else if (book.rank < book.rankLastWeek) {
-          book.rankIcon = '⇩';
-        } else {
-          book.rankIcon = '(new)';
+          // Work out which direction the book has moved in the charts, i.e. up down same or new entry
+          let rankIcon;
+          if (book.rankLastWeek === 0) {
+            book.rankIcon = '(new)';
+          } else if (book.rank > book.rankLastWeek) {
+            book.rankIcon = '⇧';
+          } else if (book.rank === book.rankLastWeek) {
+            book.rankIcon = '-';
+          } else if (book.rank < book.rankLastWeek) {
+            book.rankIcon = '⇩';
+          } else {
+            book.rankIcon = '(new)';
+          }
+
+          findValidUrl(book.isbns, (url) => {
+            setTimeout(() => {
+              book.imageURL = url;
+              books.push(book);
+            }, 500);
+          });
         }
-
-        findValidUrl(book.isbns, (url) => {
-          setTimeout(() => {
-            book.imageURL = url;
-            books.push(book);
-          }, 500);
-        });
       }
       setTimeout(() => {
         books.sort((a, b) => ((a.rank > b.rank) ? 1 : -1));
